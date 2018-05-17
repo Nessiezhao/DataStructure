84ac198dfa5cd7657ec1eee2cb53fc3e4d55d362
Nessiezhao <Nessie_zhao@163.com>
2018-05-16 20:02:41 -0700

    add heap

--- Struct/heap/heap.h
+++ Struct/heap/heap.h
@@ -1,7 +1,23 @@
 #pragma once
+#include<stddef.h>
 #define HeapMaxSize 1024
 typedef char HeapType;
 
 //如果a和b满足比较关系，返回1
 //如果a和b不满足比较关系，返回0
 //所谓的比较关系，对于小堆来说就是a<b
+//对于大堆来说，就是a>b
+typedef int (*Compare)(HeapType a,HeapType b);
+
+typedef struct Heap
+{
+    HeapType data[HeapMaxSize];
+    size_t size;
+    Compare cmp;
+}Heap;
+//在对堆进行初始化的时候，决定堆是大堆还是小堆
+void HeapInit(Heap* heap,Compare cmp);
+
+void HeapDestroy(Heap* heap);
+
+void HeapInsert(Heap* heap,HeapType value);
