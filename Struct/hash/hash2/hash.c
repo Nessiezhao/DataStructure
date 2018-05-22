d2c4e6c1bd84335fb3491a73db6fceeac1d63122
Nessiezhao <Nessie_zhao@163.com>
2018-05-21 23:25:22 -0700

    add hash2

--- Struct/hash/hash2/hash.c
+++ Struct/hash/hash2/hash.c
@@ -1,11 +1,42 @@
 #include"hash.h"
 #include <stdio.h>
+#include<stddef.h>
+
+size_t HashFuncDefault(KeyType key)
+{
+    return key % HashMaxSize;
+}
+void HashInit(HashTable* ht,HashFunc hash_func)
+{
+    if(ht == NULL)
+    {
+        return;
+    }
+    ht->size = 0;
+    ht->func = hash_func;
+    size_t i = 0;
+    for(;i < HashMaxSize ;++i)
+    {
+        ht->data[i] = NULL;
+    }
+    return;
+}
 ///////////////////////////////////////////////////////////////////
 //以下为测试代码
 ///////////////////////////////////////////////////////////////////
 #if 1
+#define TEST_HEADER printf("\n================================%s===============================\n",__FUNCTION__);
+void TestInit()
+{
+    TEST_HEADER;
+    HashTable ht;
+    HashInit(&ht,HashFuncDefault);
+    printf("size expected 0,actual %lu\n",ht.size);
+    printf("func expected %p,actual %p\n",HashFuncDefault,ht.func);
+}
 int main()
 {
+    TestInit();
     return 0;
 }
 #endif
--- Struct/hash/hash2/hash.h
+++ Struct/hash/hash2/hash.h
@@ -1,1 +1,29 @@
 #pragma once
+#include<stdio.h>
+#include<stddef.h>
+
+#define HashMaxSize 1000
+
+typedef int KeyType;
+typedef int ValType;
+
+typedef size_t (*HashFunc)(KeyType key);
+
+//此结构体相当于是一个链表的节点
+typedef struct HashElem
+{
+    KeyType key;
+    ValType value;
+    struct HashElem* next;
+}HashElem;
+
+typedef struct HashTable
+{
+    //如果我们的hash桶上的链表是一个不带头节点的链表（保存指针）
+    //类型就用HashElem*
+    //如果我们的hash桶上的链表是一个带头节点的链表（保存节点）
+    //类型就用HashElem
+    HashElem* data[HashMaxSize];
+    size_t size;
+    HashFunc func;
+}HashTable;
