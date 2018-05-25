a4b8c2b96760754dafc2d10fd94872e98051e26d
Nessiezhao <Nessie_zhao@163.com>
2018-05-24 20:05:07 -0700

    add bloom_filter

--- Struct/hash/bloom_filter/hash_func.c
+++ Struct/hash/bloom_filter/hash_func.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include<stddef.h>
+
+size_t BKDRHash(const char* str)
+{
+    size_t hash = 0;
+    while(size_t ch = (size_t)*str++)
+    {
+        hash = hash * 131 +ch;
+    }
+    return hash;
+}
