2c9bbe3718c65cb97f3576b5f0b1f5f6334eb772
Nessiezhao <Nessie_zhao@163.com>
2018-06-06 12:01:09 +0800

    add search_tree

--- Struct/seach_tree/search_tree.h
+++ Struct/seach_tree/search_tree.h
@@ -8,3 +8,9 @@ typedef struct SearchNode
     struct SearchNode* rchild;
 }SearchNode;
 
+void SearchTreeInit(SearchNode** pRoot);
+void SearchTreeDestroy(SearchNode** pRoot);
+void SearchTreeInsert(SearchNode** pRoot,SearchNodeType to_insert);
+void PreOrder(SearchNode* root);
+void Inorder(SearchNode* root);
+SearchNode* SearchTreeFind(SearchNode* root,SearchNodeType to_find);
