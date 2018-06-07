86293666cf80e4c8ea3acf2312ffac390c556649
Nessiezhao <Nessie_zhao@163.com>
2018-06-07 11:26:09 +0800

    add search_tree

--- Struct/seach_tree/search_tree.h
+++ Struct/seach_tree/search_tree.h
@@ -14,3 +14,6 @@ void SearchTreeInsert(SearchNode** pRoot,SearchNodeType to_insert);
 void PreOrder(SearchNode* root);
 void Inorder(SearchNode* root);
 SearchNode* SearchTreeFind(SearchNode* root,SearchNodeType to_find);
+void SearchTreeRemove(SearchNode** pRoot,SearchNodeType to_remove);
+void SearchTreeInsertByLoop(SearchNode** pRoot,SearchNodeType to_insert);
+SearchNode* SearchTreeFindByLoop(SearchNode* root,SearchNodeType to_find);
